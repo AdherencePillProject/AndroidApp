148c42b49c0e0f193ad0dad69978f96240386ecd
Stanley <Stanley@dhcp-10-105-147-233.wireless.northwestern.private>
2016-04-26 20:07:29 -0500

    send image to Parse

--- app/src/main/java/com/example/ilya/smartcap_v31b/SendToParseActivity.java
+++ app/src/main/java/com/example/ilya/smartcap_v31b/SendToParseActivity.java
@@ -5,20 +5,28 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.io.ByteArrayOutputStream;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -27,6 +35,12 @@ import java.util.Locale;
 
 public class SendToParseActivity extends AppCompatActivity {
     private static final String TAG = "SendToParseActivity";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,57 +50,26 @@ public class SendToParseActivity extends AppCompatActivity {
         Log.v(TAG, String.valueOf(conn));
         Parse.initialize(this, "BDo39lSOtPuBwDfq0EBDgIjTzztIQE38Fuk03EcR", "6exCVtTYC6JhQP6gw1OFByyP2RRq5McznAsoQ3Gq");
         ParseUser.enableAutomaticUser();
+//        Date now = new Date();
+//        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+//        ParseObject testObject = new ParseObject("Bottle");
+//        testObject.put("UUID", timestamp);
+//        testObject.saveInBackground();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ackme_icon);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] image = stream.toByteArray();
+        ParseFile file = new ParseFile("ackme_icon.png", image);
+        file.saveInBackground();
+        ParseObject testImageObject = new ParseObject("Image");
+        testImageObject.put("image", file);
         Date now = new Date();
         String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
         ParseObject testObject = new ParseObject("Bottle");
-        Log.v(TAG, timestamp);
-        testObject.put("UUID", timestamp);
-        testObject.saveInBackground();
-
-
-
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.IMG_20160216_215228925);
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-//        byte[] image = stream.toByteArray();
-//        ParseFile file = new ParseFile("smile.png", image);
-//        file.saveInBackground();
-//        ParseObject testObjectImage = new ParseObject("Image");
-//        testObjectImage.put("image", file);
-//        testObjectImage.saveInBackground();
-
-
-
-
-
-//        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-//        NetworkInfo ni = cm.getActiveNetworkInfo();
-//        if ((ni != null) && (ni.isConnected())) {
-//            if (!ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
-//                // If we have a network connection and a current
-//                // logged in user, sync the todos
-//                Toast.makeText(
-//                        getApplicationContext(),
-//                        "Connected",
-//                        Toast.LENGTH_LONG).show();
-//            } else {
-//                // If we have a network connection but no logged in user, direct
-//                // the person to log in or sign up.
-//                Toast.makeText(
-//                        getApplicationContext(),
-//                        "No logged",
-//                        Toast.LENGTH_LONG).show();
-//                //ParseLoginBuilder builder = new ParseLoginBuilder(this);
-//                //startActivityForResult(builder.build(), LOGIN_ACTIVITY_CODE);
-//            }
-//        } else {
-//            // If there is no connection, let the user know the sync didn't happen
-//            Toast.makeText(
-//                    getApplicationContext(),
-//                    "Your device appears to be offline. Some todos may not have been synced to Parse.",
-//                    Toast.LENGTH_LONG).show();
-//        }
-//        Log.v(TAG, "dddddddd");
+        testImageObject.put("UUID", timestamp);
+        testImageObject.saveInBackground();
+        
     }
 
     private boolean isNetworkConnected() {
@@ -97,5 +80,45 @@ public class SendToParseActivity extends AppCompatActivity {
             return false;
         } else
             return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SendToParse Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.ilya.smartcap_v31b/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SendToParse Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.ilya.smartcap_v31b/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
